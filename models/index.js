4247235555270025f53c499fcaaf298e2a6b061b
Yoav Katz <128583921+YoavKatz99@users.noreply.github.com>
2025-04-03 19:49:10 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.Promise = Promise;
 
